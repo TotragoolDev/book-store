3a5444e7bc630841c115b158fc3aa0c7f75ba3ed
TotragoolDev <totragooldev@gmail.com>
2026-09-04 17:16:12 +0700

    commit

--- models/user.model.js
+++ models/user.model.js
@@ -2,24 +2,10 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  role: {
-    type: String, 
-    default: 'user'
-  },
-  status: {
-    type: String,
-    enum: ['pending', 'approved', 'rejected'],
-    default: 'pending'
-  }
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  role: {type: String, default: 'user'},
+  status: {type: String,enum: ['pending', 'approved', 'rejected'],default: 'pending'}
 }, {
   timestamps: true
 })
